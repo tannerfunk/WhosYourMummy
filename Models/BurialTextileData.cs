7cbede3579eeeb657da57fafc8e9a53a25bd8bab
Cameron Delgrosso <cam4del@byu.edu>
2023-04-13 14:29:53 -0600

    455 Pages

--- Models/BurialTextileData.cs
+++ Models/BurialTextileData.cs
@@ -6,14 +6,11 @@ namespace WhosYourMummy.Models
     public class BurialTextileData
     {
         [Key]
-        
         public long BurialId { get; set; }
         public long TextileId { get; set; }
         public string TextileName { get; set; }
-
         public string? ExcavationYear { get; set; }
         public string? Area { get; set; }
-
         public string? Sex { get; set; }
 
     }
